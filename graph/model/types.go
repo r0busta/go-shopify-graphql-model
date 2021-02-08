3a14a49a56eada8889b9ec9b7c020d1c2facf014
Kirill Zhuchkov <643378+r0busta@users.noreply.github.com>
2021-02-08 19:37:57 +0100

    Add convenience type functions

---
+++ graph/model/types.go
@@ -0,0 +1,19 @@
+package model
+
+import "gopkg.in/guregu/null.v4"
+
+func NewNullString(v null.String) *null.String {
+	return &v
+}
+
+func NewBool(v bool) *bool {
+	return &v
+}
+
+func NewInt(v int) *int {
+	return &v
+}
+
+func NewFloat64(v float64) *float64 {
+	return &v
+}
