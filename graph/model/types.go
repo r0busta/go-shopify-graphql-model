c4b181fcab5102e576eed333ae1ef9c6a594f696
Vincent Luan <vincentluan@gempages.help>
2023-07-26 16:48:49 +0700

    Added custom UnmarshalJSON function to MediaEdge and MediaConnection

--- graph/model/types.go
+++ graph/model/types.go
@@ -1,23 +1,51 @@
 package model
 
-import "gopkg.in/guregu/null.v4"
+import (
+	"fmt"
+	"reflect"
+	"regexp"
+
+	"gopkg.in/guregu/null.v4"
+)
+
+var gidRegex *regexp.Regexp
+
+func init() {
+	gidRegex = regexp.MustCompile(`^gid://shopify/(\w+)/\d+$`)
+}
 
 func NewNullString(v null.String) *null.String {
 	return &v
 }
-
 func NewString(v string) *string {
 	return &v
 }
-
 func NewBool(v bool) *bool {
 	return &v
 }
-
 func NewInt(v int) *int {
 	return &v
 }
-
 func NewFloat64(v float64) *float64 {
 	return &v
+}
+
+func concludeObjectType(gid string) (reflect.Type, error) {
+	submatches := gidRegex.FindStringSubmatch(gid)
+	if len(submatches) != 2 {
+		return reflect.TypeOf(nil), fmt.Errorf("malformed gid=`%s`", gid)
+	}
+	resource := submatches[1]
+	switch resource {
+	case "MediaImage":
+		return reflect.TypeOf(MediaImage{}), nil
+	case "Video":
+		return reflect.TypeOf(Video{}), nil
+	case "Model3d":
+		return reflect.TypeOf(Model3d{}), nil
+	case "ExternalVideo":
+		return reflect.TypeOf(ExternalVideo{}), nil
+	default:
+		return reflect.TypeOf(nil), fmt.Errorf("`%s` not implemented type", resource)
+	}
 }
